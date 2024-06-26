fb3b64b8aa8cefb0c524db1bbb5d6ef44c0a8eda
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-06-26 20:29:02 +0200

    chore: replace package name for each app

--- packages/migrate/index.ts
+++ packages/migrate/index.ts
@@ -1,4 +1,5 @@
 import degit from 'degit'
+import { replaceInFileSync, type ReplaceInFileConfig } from 'replace-in-file'
 import { apps } from '../index/src/apps'
 
 const appPromises = Object.values(apps)
@@ -53,5 +54,62 @@ Promise.all(appPromises)
       }).catch((e) => {
         console.error(e)
       })
+  })
+  .then(() => {
+    const dry = false
+    const ignore = [
+      './node_modules/**',
+      './**/node_modules/**',
+      '../../apps/**/node_modules/**',
+      '../../packages/**/node_modules/**',
+    ]
+
+    const tasks: ReplaceInFileConfig[] = [
+      {
+        // rename package.json `name`
+        from: /"(name)": "([\w-]+)",/gm,
+        to: (...args) => {
+          const file = args.pop()
+          const folderName = file?.match(/apps\/([\w-]+)\/package.json$/)?.[1]
+          console.log(file, folderName)
+          return `"name": "${folderName}",`
+        },
+        files: [
+          '../../apps/**/package.json'
+        ],
+      },
+      {
+        // remove `vite.config.mts` from 'tsconfig.json'
+        from: [
+          ',\n    "vite.config.mts"',
+          '\n    "*.config.mts",',
+          '\n    "*.config.ts",',
+          '\n    "*.config.js",',
+          '\n    "*.config.cjs",',
+        ],
+        to: '',
+        files: [
+          '../../apps/**/tsconfig.json',
+          '../../packages/**/tsconfig.json'
+        ],
+      }
+    ]
+
+    const results = tasks.flatMap(task => replaceInFileSync({
+      dry,
+      ignore,
+      ...task
+    }))
+
+    const filteredResults = results.filter(r => r.hasChanged).map(r => r.file)
+    let uniqueFilteredResults = [...new Set(filteredResults)]
+
+    if (uniqueFilteredResults.length > 0) {
+      console.group('\nUpdating source code:',)
+      uniqueFilteredResults.forEach(r => {
+        console.info('→', r)
+      })
+      console.groupEnd()
+    }
   })
   .then(() => console.log('\ndone!'))
