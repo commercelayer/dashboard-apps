a4f8e221ce2cf11fe04ec1029caafc98197a989b
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-08-30 22:07:37 +0200

    chore(orders): auto-select customer on new order refresh

--- apps/orders/src/components/NewOrder/SelectCustomerComponent.tsx
+++ apps/orders/src/components/NewOrder/SelectCustomerComponent.tsx
@@ -1,5 +1,6 @@
 import {
   HookedInputSelect,
+  SkeletonTemplate,
   useCoreApi,
   useCoreSdkProvider,
   type InputSelectValue
@@ -13,45 +14,54 @@ export function SelectCustomerComponent(): JSX.Element {
 
   const inputValue = watch('customer_email')
 
-  const { data: customers } = useCoreApi('customers', 'list', [
-    {
-      filters: {
-        email_not_eq: inputValue
+  const { data: customers, isLoading: isLoadingCustomers } = useCoreApi(
+    'customers',
+    'list',
+    [
+      {
+        filters: {
+          email_not_eq: inputValue
+        }
       }
-    }
-  ])
+    ]
+  )
 
-  const { data: selectedCustomer } = useCoreApi('customers', 'list', [
-    {
-      filters: {
-        email_eq: inputValue
+  const { data: selectedCustomer, isLoading: isLoadingSelectedCustomer } =
+    useCoreApi('customers', 'list', [
+      {
+        filters: {
+          email_eq: inputValue
+        }
       }
-    }
-  ])
+    ])
+
+  const isLoading = isLoadingCustomers || isLoadingSelectedCustomer
 
   const initialValues = toInputSelectValues(
     selectedCustomer?.length === 1 ? selectedCustomer : []
   ).concat(toInputSelectValues(customers ?? []))
 
   return (
-    <HookedInputSelect
-      name='customer_email'
-      label='Email *'
-      placeholder='Search or add email'
-      hint={{ text: "The customer's email for this order." }}
-      isCreatable
-      menuFooterText={
-        customers != null && customers.meta.recordCount > 25
-          ? 'Search or add email.'
-          : undefined
-      }
-      initialValues={initialValues}
-      loadAsyncValues={async (email) => {
-        const customers = await sdkClient.customers.list(getParams({ email }))
+    <SkeletonTemplate isLoading={isLoading} delayMs={0}>
+      <HookedInputSelect
+        name='customer_email'
+        label='Email *'
+        placeholder='Search or add email'
+        hint={{ text: "The customer's email for this order." }}
+        isCreatable
+        menuFooterText={
+          customers != null && customers.meta.recordCount > 25
+            ? 'Search or add email.'
+            : undefined
+        }
+        initialValues={initialValues}
+        loadAsyncValues={async (email) => {
+          const customers = await sdkClient.customers.list(getParams({ email }))
 
-        return toInputSelectValues(customers)
-      }}
-    />
+          return toInputSelectValues(customers)
+        }}
+      />
+    </SkeletonTemplate>
   )
 }
 
