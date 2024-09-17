bca6c9bda556202d18bd0c38f59dedae3484650e
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2024-09-17 12:40:09 +0200

    fix: add gift card form

---
+++ apps/gift_cards/src/components/Form.tsx
@@ -0,0 +1,206 @@
+import { appRoutes } from '#data/routes'
+import {
+  Button,
+  Grid,
+  HookedForm,
+  HookedInput,
+  HookedInputCheckbox,
+  HookedInputCurrency,
+  HookedInputDate,
+  HookedMarketWithCurrencySelector,
+  HookedValidationApiError,
+  Icon,
+  Section,
+  Spacer,
+  Tooltip,
+  type CurrencyCode
+} from '@commercelayer/app-elements'
+import type { GiftCard } from '@commercelayer/sdk'
+import { zodResolver } from '@hookform/resolvers/zod'
+import isEmpty from 'lodash/isEmpty'
+import { useState, type FC } from 'react'
+import { useForm } from 'react-hook-form'
+import { useLocation } from 'wouter'
+import { z } from 'zod'
+
+const formSchema = z
+  .object({
+    market: z.string(),
+    currency_code: z.string(),
+    balance_cents: z.number().int().min(0),
+    balance_max_cents: z.number().int().optional().nullable(),
+    expires_at: z
+      .date({
+        required_error: 'Please enter a valid date',
+        invalid_type_error: 'Please enter a valid date'
+      })
+      .optional()
+      .nullable(),
+    recipient_email: z.string().optional().nullable(),
+    image_url: z.string().trim().url().optional().nullable().or(z.literal('')),
+    single_use: z.boolean(),
+    rechargeable: z.boolean()
+  })
+  .superRefine((values, ctx) => {
+    if (isEmpty(values.currency_code) && isEmpty(values.market)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['currency_code'],
+        message: 'Please select a currency'
+      })
+    }
+  })
+
+type GiftCardFormValues = z.infer<typeof formSchema>
+
+const makeDefaultValue = (giftCart?: GiftCard): GiftCardFormValues => {
+  return {
+    market: giftCart?.market?.id ?? '',
+    currency_code: giftCart?.currency_code ?? 'USD',
+    balance_cents: giftCart?.balance_cents ?? 0,
+    // @ts-expect-error wrong type from SDK
+    balance_max_cents: giftCart?.balance_max_cents ?? null,
+    expires_at:
+      giftCart?.expires_at != null ? new Date(giftCart.expires_at) : null,
+    recipient_email: giftCart?.recipient_email ?? null,
+    image_url: giftCart?.image_url ?? null,
+    single_use: giftCart?.single_use ?? false,
+    rechargeable: giftCart?.rechargeable ?? false
+  }
+}
+
+export const Form: FC<{
+  giftCard?: GiftCard
+  onSubmit: (formValues: GiftCardFormValues) => Promise<GiftCard>
+}> = ({ giftCard, onSubmit }) => {
+  const [apiError, setApiError] = useState<any>()
+  const [, setLocation] = useLocation()
+
+  const methods = useForm({
+    defaultValues: makeDefaultValue(giftCard),
+    resolver: zodResolver(formSchema)
+  })
+
+  const isNew = giftCard?.id == null
+  const currencyCode = methods.watch('currency_code') as CurrencyCode
+
+  return (
+    <HookedForm
+      {...methods}
+      onSubmit={(formValues) => {
+        onSubmit(formValues)
+          .then((res) => {
+            setLocation(appRoutes.details.makePath({ giftCardId: res.id }))
+          })
+          .catch((error) => {
+            setApiError(error)
+            throw error
+          })
+      }}
+    >
+      <Section title='Balance'>
+        <Spacer top='6'>
+          <HookedMarketWithCurrencySelector
+            label='Market *'
+            fieldNameMarket='market'
+            fieldNameCurrencyCode='currency_code'
+            hint={{
+              text: 'Market where the card can be used.'
+            }}
+          />
+        </Spacer>
+        <Grid columns='2'>
+          <Spacer top='6'>
+            <HookedInputCurrency
+              name='balance_cents'
+              label='Balance *'
+              currencyCode={currencyCode}
+              hint={{
+                text: 'Value of the gift card'
+              }}
+            />
+          </Spacer>
+          <Spacer top='6'>
+            <HookedInputCurrency
+              name='balance_max_cents'
+              label='Max balance'
+              currencyCode={currencyCode}
+              hint={{
+                text: 'Maximum value of the gift card'
+              }}
+            />
+          </Spacer>
+        </Grid>
+      </Section>
+
+      <Spacer top='14'>
+        <Section title='Additional info'>
+          <Spacer top='6'>
+            <HookedInputDate
+              name='expires_at'
+              label='Expiration date'
+              showTimeSelect
+              hint={{
+                text: 'Up to when the card can be used.'
+              }}
+            />
+          </Spacer>
+          <Spacer top='6'>
+            <HookedInput
+              name='recipient_email'
+              label='Customer email'
+              hint={{
+                text: 'The customer associated with the card'
+              }}
+            />
+          </Spacer>
+          <Spacer top='6'>
+            <HookedInput
+              name='image_url'
+              label='Image URL'
+              hint={{
+                text: 'A valid image URL, hosted anywhere.'
+              }}
+            />
+          </Spacer>
+        </Section>
+      </Spacer>
+
+      <Spacer top='14'>
+        <Section title='Options'>
+          <Spacer top='6'>
+            <HookedInputCheckbox name='rechargeable'>
+              The card is rechargeable
+            </HookedInputCheckbox>
+          </Spacer>
+          <Spacer top='2'>
+            <HookedInputCheckbox name='single_use'>
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '8px',
+                  alignItems: 'center'
+                }}
+              >
+                The card can only be used once
+                <Tooltip
+                  label={<Icon name='info' />}
+                  content='Single-use cards are redeemed on first use, regardless of balance.'
+                />
+              </div>
+            </HookedInputCheckbox>
+          </Spacer>
+        </Section>
+      </Spacer>
+
+      <Spacer top='14'>
+        <Button type='submit' className='w-full'>
+          {isNew ? 'Create' : 'Update'}
+        </Button>
+        <Spacer top='2'>
+          <HookedValidationApiError apiError={apiError} />
+        </Spacer>
+      </Spacer>
+    </HookedForm>
+  )
+}
