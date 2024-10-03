e9097b32b26326509875b11964db1f94d33ee613
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-10-03 11:58:57 +0200

    fix: add missing form behavior to stock transfers timeline

--- apps/stock_transfers/src/components/StockTransferTimeline.tsx
+++ apps/stock_transfers/src/components/StockTransferTimeline.tsx
@@ -1,9 +1,12 @@
-import { type referenceOrigins } from '#data/attachments'
+import { referenceOrigins } from '#data/attachments'
 import { isMockedId } from '#mocks'
 import {
+  Section,
+  Spacer,
   Text,
   Timeline,
   useCoreApi,
+  useCoreSdkProvider,
   useTokenProvider,
   withSkeletonTemplate,
   type TimelineEvent
@@ -20,56 +23,104 @@ export const StockTransferTimeline = withSkeletonTemplate<{
     onMessage?: (attachment: Attachment) => void
     referenceOrigin: typeof referenceOrigins.appStockTransfersNote
   }
-}>(({ stockTransferId, refresh, isLoading: isExternalLoading }) => {
-  const fakeStockTransferId = 'fake-NMWYhbGorj'
-  const {
-    data: stockTransfer,
-    isLoading,
-    mutate: mutateStockTransfer
-  } = useCoreApi(
-    'stock_transfers',
-    'retrieve',
-    stockTransferId == null ||
-      isEmpty(stockTransferId) ||
-      isMockedId(stockTransferId)
-      ? null
-      : [stockTransferId],
-    {
-      fallbackData: {
-        type: 'stock_transfers',
-        id: fakeStockTransferId,
-        status: 'upcoming',
-        quantity: 1,
-        created_at: '2020-05-16T11:06:02.074Z',
-        updated_at: '2020-05-16T14:18:35.572Z'
-      } satisfies StockTransfer
-    }
-  )
-
-  const [events] = useTimelineReducer(stockTransfer)
-  const { user } = useTokenProvider()
-
-  useEffect(
-    function refreshStockTransfer() {
-      if (refresh === true) {
-        void mutateStockTransfer()
+}>(
+  ({
+    stockTransferId,
+    attachmentOption,
+    refresh,
+    isLoading: isExternalLoading
+  }) => {
+    const fakeStockTransferId = 'fake-NMWYhbGorj'
+    const {
+      data: stockTransfer,
+      isLoading,
+      mutate: mutateStockTransfer
+    } = useCoreApi(
+      'stock_transfers',
+      'retrieve',
+      stockTransferId == null ||
+        isEmpty(stockTransferId) ||
+        isMockedId(stockTransferId)
+        ? null
+        : [stockTransferId],
+      {
+        fallbackData: {
+          type: 'stock_transfers',
+          id: fakeStockTransferId,
+          status: 'upcoming',
+          quantity: 1,
+          created_at: '2020-05-16T11:06:02.074Z',
+          updated_at: '2020-05-16T14:18:35.572Z'
+        } satisfies StockTransfer
       }
-    },
-    [refresh]
-  )
+    )
 
-  return (
-    <Timeline
-      isLoading={
-        isExternalLoading === true ||
-        isLoading ||
-        stockTransfer.id === fakeStockTransferId
-      }
-      events={events}
-      timezone={user?.timezone}
-    />
-  )
-})
+    const [events] = useTimelineReducer(stockTransfer)
+    const { sdkClient } = useCoreSdkProvider()
+    const { user } = useTokenProvider()
+
+    useEffect(
+      function refreshStockTransfer() {
+        if (refresh === true) {
+          void mutateStockTransfer()
+        }
+      },
+      [refresh]
+    )
+
+    return (
+      <Section title='Timeline'>
+        <Spacer top='8'>
+          <Timeline
+            isLoading={
+              isExternalLoading === true ||
+              isLoading ||
+              stockTransfer.id === fakeStockTransferId
+            }
+            events={events}
+            timezone={user?.timezone}
+            onKeyDown={(event) => {
+              if (event.code === 'Enter' && event.currentTarget.value !== '') {
+                if (
+                  attachmentOption?.referenceOrigin == null ||
+                  ![referenceOrigins.appStockTransfersNote].includes(
+                    attachmentOption.referenceOrigin
+                  )
+                ) {
+                  console.warn(
+                    `Cannot create the attachment: "referenceOrigin" is not valid.`
+                  )
+                  return
+                }
+
+                if (user?.displayName == null || isEmpty(user.displayName)) {
+                  console.warn(
+                    `Cannot create the attachment: token does not contain a valid "user".`
+                  )
+                  return
+                }
+
+                void sdkClient.attachments
+                  .create({
+                    reference_origin: attachmentOption.referenceOrigin,
+                    name: user.displayName,
+                    description: event.currentTarget.value,
+                    attachable: { type: 'returns', id: stockTransfer.id }
+                  })
+                  .then((attachment) => {
+                    void mutateStockTransfer()
+                    attachmentOption?.onMessage?.(attachment)
+                  })
+
+                event.currentTarget.value = ''
+              }
+            }}
+          />
+        </Spacer>
+      </Section>
+    )
+  }
+)
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const useTimelineReducer = (stockTransfer: StockTransfer) => {
