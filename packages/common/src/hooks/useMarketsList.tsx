6bb73d0fea1c3ab8357f7f1ff944452acc314b0b
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-10-04 15:34:37 +0200

    fix: consider only markets that are not private for links

--- packages/common/src/hooks/useMarketsList.tsx
+++ packages/common/src/hooks/useMarketsList.tsx
@@ -33,7 +33,7 @@ export function useMarketsList({ settings }: Props): {
     'list',
     [
       {
-        filters: { customer_group_null: true },
+        filters: { customer_group_null: true, private_true: false },
         fields: ['id', 'name'],
         sort: ['name'],
         pageNumber,
