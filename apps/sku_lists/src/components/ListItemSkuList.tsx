d2288a2de9ffab2772ef72c931a41e5376aa0490
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-10-10 10:25:17 +0200

    chore: fix font weight of sku lists list item

--- apps/sku_lists/src/components/ListItemSkuList.tsx
+++ apps/sku_lists/src/components/ListItemSkuList.tsx
@@ -26,7 +26,7 @@ export const ListItemSkuList = withSkeletonTemplate<Props>(
         }}
       >
         <div>
-          <Text weight='bold'>{resource.name}</Text>
+          <Text weight='semibold'>{resource.name}</Text>
         </div>
         <Icon name='caretRight' />
       </ListItem>
