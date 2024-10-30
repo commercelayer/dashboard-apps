169ff1d4106be3b2e3a2b32ca2ee90f5f4e8b7d5
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-10-30 16:06:23 +0100

    chore(subscriptions): reorganize routes to have /list base

--- apps/subscriptions/src/App.tsx
+++ apps/subscriptions/src/App.tsx
@@ -3,7 +3,7 @@ import { appRoutes } from '#data/routes'
 import { Filters } from '#pages/Filters'
 import { SubscriptionsList } from '#pages/SubscriptionsList'
 import type { FC } from 'react'
-import { Route, Router, Switch } from 'wouter'
+import { Redirect, Route, Router, Switch } from 'wouter'
 
 interface AppProps {
   routerBase?: string
@@ -13,6 +13,9 @@ export const App: FC<AppProps> = ({ routerBase }) => {
   return (
     <Router base={routerBase}>
       <Switch>
+        <Route path={appRoutes.home.path}>
+          <Redirect to={appRoutes.list.path} />
+        </Route>
         <Route path={appRoutes.list.path}>
           <SubscriptionsList />
         </Route>
