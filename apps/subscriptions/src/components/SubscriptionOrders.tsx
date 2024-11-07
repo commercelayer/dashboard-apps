8d1d4da4b31d4d4b55cceb9748f940a9368a52ca
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2024-11-07 12:45:53 +0100

    feat(subscriptions): add See all button to orders list in detail page

--- apps/subscriptions/src/components/SubscriptionOrders.tsx
+++ apps/subscriptions/src/components/SubscriptionOrders.tsx
@@ -1,12 +1,18 @@
 import { ListItemSubscriptionOrder } from '#components/ListItemSubscriptionOrder'
+import { appRoutes } from '#data/routes'
 import {
+  Button,
+  Icon,
   Section,
+  Table,
   Td,
+  Th,
   Tr,
   useResourceList,
   withSkeletonTemplate
 } from '@commercelayer/app-elements'
 import type { OrderSubscription } from '@commercelayer/sdk'
+import { useLocation } from 'wouter'
 
 interface Props {
   subscription: OrderSubscription
@@ -14,39 +20,63 @@ interface Props {
 
 export const SubscriptionOrders = withSkeletonTemplate<Props>(
   ({ subscription }) => {
-    const { ResourceList } = useResourceList({
+    const { list, meta } = useResourceList({
       type: 'orders',
       query: {
         filters: {
           order_subscription_id_eq: subscription.id,
           status_eq: 'placed'
         },
-        sort: ['-placed_at']
+        sort: ['-placed_at'],
+        pageSize: 5
       }
     })
 
+    const showAll = meta != null ? meta.pageCount > 1 : false
+    const [, setLocation] = useLocation()
+    const noResults = meta != null ? meta.recordCount === 0 : true
+
     return (
-      <Section title='Recurring orders' border='none'>
-        <ResourceList
-          variant='table'
-          headings={[
-            {
-              label: 'DATE'
-            },
-            {
-              label: 'ORDER',
-              align: 'left'
-            },
-            {
-              label: 'PAYMENT STATUS'
-            }
-          ]}
-          emptyState={
+      <Section
+        title={`Recurring orders · ${meta?.recordCount}`}
+        border='none'
+        actionButton={
+          showAll && (
+            <Button
+              variant='secondary'
+              size='mini'
+              onClick={() => {
+                setLocation(
+                  appRoutes.orders.makePath({ subscriptionId: subscription.id })
+                )
+              }}
+              alignItems='center'
+            >
+              <Icon name='eye' size={16} />
+              See all
+            </Button>
+          )
+        }
+      >
+        <Table
+          thead={
             <Tr>
-              <Td colSpan={3}>no results</Td>
+              <Th>Date</Th>
+              <Th>Order</Th>
+              <Th>Result</Th>
             </Tr>
           }
-          ItemTemplate={ListItemSubscriptionOrder}
+          tbody={
+            noResults ? (
+              <Tr>
+                <Td colSpan={3}>no results</Td>
+              </Tr>
+            ) : (
+              list?.map((order, idx) => (
+                <ListItemSubscriptionOrder resource={order} key={idx} />
+              ))
+            )
+          }
         />
       </Section>
     )
