7a309ab44a512865a686336425b4ec5c0eb1325c
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2024-12-12 10:47:49 +0100

    fix(price_lists): update price tiers form schema and default values to better work with input of type number

--- apps/price_lists/src/components/PriceTierForm/schema.ts
+++ apps/price_lists/src/components/PriceTierForm/schema.ts
@@ -1,15 +1,34 @@
 import { isFalsy } from '#utils/isFalsy'
 import { z } from 'zod'
 
-export const priceTierFormSchema = z
-  .object({
-    id: z.string().optional(),
-    name: z.string().min(1),
-    currency_code: z.string().min(1),
+const baseSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(1),
+  currency_code: z.string().min(1),
+  price: z
+    .number({
+      message: 'Please enter a valid price',
+      invalid_type_error: 'Please enter a valid price'
+    })
+    .min(0)
+})
+
+export const priceTierVolumeFormSchema = baseSchema
+  .extend({
+    type: z.literal('volume'),
+    up_to: z.preprocess((value) => {
+      return Number.isNaN(value) ? null : value
+    }, z.number().min(0).nullish())
+  })
+  .passthrough()
+
+export const priceTierFrequencyFormSchema = baseSchema
+  .extend({
+    type: z.literal('frequency'),
     up_to: z.string(),
-    up_to_days: z.string().optional(),
-    price: z.number(),
-    type: z.string()
+    up_to_days: z.preprocess((value) => {
+      return Number.isNaN(value) ? null : value
+    }, z.number().min(0).nullish())
   })
   .passthrough()
   .superRefine((data, ctx) => {
