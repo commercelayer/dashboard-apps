69261468859eb0f56590131c2adc2cfec85325ad
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-04-15 16:00:52 +0200

    chore: cleanup appList

--- packages/index/src/appList.js
+++ packages/index/src/appList.js
@@ -7,7 +7,7 @@
 
 /**
  * A number, or a string containing a number.
- * @typedef {{ name: string; slug: AllowedAppSlug; icon: import('@commercelayer/app-elements').IconProps['name']; repositoryName: string }} App
+ * @typedef {{ name: string; slug: AllowedAppSlug; icon: import('@commercelayer/app-elements').IconProps['name'] }} App
  */
 
 /** @type {Record<AllowedAppSlug, App>} */
@@ -15,103 +15,86 @@ export const apps = {
   orders: {
     name: 'Orders',
     slug: 'orders',
-    icon: 'shoppingBag',
-    repositoryName: 'app-orders'
+    icon: 'shoppingBag'
   },
   shipments: {
     name: 'Shipments',
     slug: 'shipments',
-    icon: 'package',
-    repositoryName: 'app-shipments'
+    icon: 'package'
   },
   customers: {
     name: 'Customers',
     slug: 'customers',
-    icon: 'users',
-    repositoryName: 'app-customers'
+    icon: 'users'
   },
   returns: {
     name: 'Returns',
     slug: 'returns',
-    icon: 'arrowUUpLeft',
-    repositoryName: 'app-returns'
+    icon: 'arrowUUpLeft'
   },
   stock_transfers: {
     name: 'Stock transfers',
     slug: 'stock_transfers',
-    icon: 'arrowsLeftRight',
-    repositoryName: 'app-stock-transfers'
+    icon: 'arrowsLeftRight'
   },
   skus: {
     name: 'SKUs',
     slug: 'skus',
-    icon: 'tShirt',
-    repositoryName: 'app-skus'
+    icon: 'tShirt'
   },
   sku_lists: {
     name: 'SKU Lists',
     slug: 'sku_lists',
-    icon: 'clipboardText',
-    repositoryName: 'app-sku-lists'
+    icon: 'clipboardText'
   },
   imports: {
     name: 'Imports',
     slug: 'imports',
-    icon: 'download',
-    repositoryName: 'app-imports'
+    icon: 'download'
   },
   exports: {
     name: 'Exports',
     slug: 'exports',
-    icon: 'upload',
-    repositoryName: 'app-exports'
+    icon: 'upload'
   },
   webhooks: {
     name: 'Webhooks',
     slug: 'webhooks',
-    icon: 'webhooksLogo',
-    repositoryName: 'app-webhooks'
+    icon: 'webhooksLogo'
   },
   tags: {
     name: 'Tags',
     slug: 'tags',
-    icon: 'tag',
-    repositoryName: 'app-tags'
+    icon: 'tag'
   },
   bundles: {
     name: 'Bundles',
     slug: 'bundles',
-    icon: 'shapes',
-    repositoryName: 'app-bundles'
+    icon: 'shapes'
   },
   gift_cards: {
     name: 'Gift cards',
     slug: 'gift_cards',
-    icon: 'gift',
-    repositoryName: 'app-gift-cards'
+    icon: 'gift'
   },
   inventory: {
     name: 'Inventory',
     slug: 'inventory',
-    icon: 'warehouse',
-    repositoryName: 'app-inventory'
+    icon: 'warehouse'
   },
   price_lists: {
     name: 'Price lists',
     slug: 'price_lists',
-    icon: 'receipt',
-    repositoryName: 'app-price-lists'
+    icon: 'receipt'
   },
   promotions: {
     name: 'Promotions',
     slug: 'promotions',
-    icon: 'seal',
-    repositoryName: 'app-promotions'
+    icon: 'seal'
   },
   subscriptions: {
     name: 'Subscriptions',
     slug: 'subscriptions',
-    icon: 'calendarCheck',
-    repositoryName: 'app-subscriptions'
+    icon: 'calendarCheck'
   }
 }
