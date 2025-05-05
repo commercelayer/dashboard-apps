fdca55e922340d8e5929f6da5cbc1cf9eebbc284
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2025-05-05 11:32:19 +0200

    fix(imports): update `gift_cards` import template

--- apps/imports/src/components/InputParser/templates/giftCards.ts
+++ apps/imports/src/components/InputParser/templates/giftCards.ts
@@ -2,12 +2,17 @@ import { type GiftCardCreate } from '@commercelayer/sdk'
 import { type CsvTagsColumn, csvTagsColumns } from './_tags'
 
 export const csvGiftCardsTemplate: Array<
-  keyof GiftCardCreate | 'market_id' | 'gift_card_recipient_id' | CsvTagsColumn
+  | keyof GiftCardCreate
+  | 'market_id'
+  | 'status'
+  | 'gift_card_recipient_id'
+  | CsvTagsColumn
 > = [
   'code',
   'currency_code',
   'balance_cents',
   'balance_max_cents',
+  'status',
   'single_use',
   'rechargeable',
   'distribute_discount',
@@ -17,5 +22,6 @@ export const csvGiftCardsTemplate: Array<
   'reference',
   'reference_origin',
   'gift_card_recipient_id',
+  'market_id',
   ...csvTagsColumns
 ]
