80af4f992289b52b01e6080b38df75be4e0d001a
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2025-07-09 10:08:48 +0200

    fix(exports): improve UI in export details page

--- apps/exports/src/components/Details/ExportDetails.tsx
+++ apps/exports/src/components/Details/ExportDetails.tsx
@@ -1,8 +1,11 @@
 import {
+  Badge,
   ListDetails,
   ListDetailsItem,
+  Tag,
   withSkeletonTemplate
 } from '@commercelayer/app-elements'
+import isEmpty from 'lodash-es/isEmpty'
 import { useExportDetailsContext } from './Provider'
 import { StatusBadge } from './StatusBadge'
 
@@ -22,20 +25,36 @@ export const ExportDetails = withSkeletonTemplate(({ isLoading }) => {
           <StatusBadge job={data} />
         </ListDetailsItem>
       ) : null}
-      {data.includes != null && data.includes.length > 0 ? (
-        <ListDetailsItem label='Includes' gutter='none'>
-          {data.includes.join(', ')}
-        </ListDetailsItem>
-      ) : null}
+
+      <ListDetailsItem label='Includes' gutter='none'>
+        {data.includes != null && data.includes.length > 0 ? (
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
+            {data.includes.map((inc) => (
+              <Tag key={inc}>{inc}</Tag>
+            ))}
+          </div>
+        ) : null}
+      </ListDetailsItem>
 
       <ListDetailsItem label='Filters' gutter='none'>
         <JsonPreview json={data.filters} />
       </ListDetailsItem>
-      <ListDetailsItem label='Dry Data' gutter='none'>
-        {data.dry_data === true ? 'true' : 'false'}
-      </ListDetailsItem>
-      <ListDetailsItem label='Format' gutter='none'>
-        {data.format}
+
+      <ListDetailsItem label='Options' gutter='none'>
+        {data.dry_data === true || !isEmpty(data.fields) ? (
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}>
+            {data.dry_data === true && (
+              <Badge variant='teal' icon='check'>
+                Importable
+              </Badge>
+            )}
+            {!isEmpty(data.fields) && (
+              <Badge variant='teal' icon='check'>
+                Simple format
+              </Badge>
+            )}
+          </div>
+        ) : null}
       </ListDetailsItem>
     </ListDetails>
   )
@@ -43,7 +62,7 @@ export const ExportDetails = withSkeletonTemplate(({ isLoading }) => {
 
 function JsonPreview({ json }: { json?: object | null }): React.JSX.Element {
   return (
-    <pre>
+    <pre className='bg-gray-50 overflow-x-auto p-4 text-sm'>
       {json != null && Object.keys(json).length > 0 ? (
         <>{JSON.stringify(json, null, 2)}</>
       ) : (
