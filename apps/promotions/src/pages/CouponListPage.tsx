035f2f1f3f2999e875ffa1dff9bcdf22c668ad49
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-09-25 15:56:42 +0200

    feat: add status filter on the promotion coupon list

--- apps/promotions/src/pages/CouponListPage.tsx
+++ apps/promotions/src/pages/CouponListPage.tsx
@@ -2,15 +2,21 @@ import { CouponRow } from '#components/CouponTable'
 import type { PageProps } from '#components/Routes'
 import { appRoutes } from '#data/routes'
 import {
+  Dropdown,
+  DropdownItem,
+  Icon,
   PageLayout,
   SearchBar,
   Spacer,
+  Text,
   useResourceList,
   useTokenProvider
 } from '@commercelayer/app-elements'
-import { useState } from 'react'
+import type { QueryFilter } from '@commercelayer/sdk'
+import { useMemo, useState } from 'react'
 import { useLocation } from 'wouter'
 
+type FilterStatus = 'all' | 'active' | 'expired' | 'never'
 function Page(
   props: PageProps<typeof appRoutes.couponList>
 ): React.JSX.Element {
@@ -19,19 +25,41 @@ function Page(
   } = useTokenProvider()
   const [, setLocation] = useLocation()
   const [searchValue, setSearchValue] = useState<string>()
+  const [filterStatus, setFilterStatus] = useState<FilterStatus>('all')
 
-  const { list, ResourceList } = useResourceList({
-    type: 'coupons',
-    query: {
+  const filterStatusLabel: Record<FilterStatus, string> = {
+    all: 'All',
+    active: 'Active',
+    expired: 'Expired',
+    never: 'Never expire'
+  }
+
+  const filterStatusList: FilterStatus[] = ['all', 'active', 'expired', 'never']
+
+  const filterStatusPredicate: Record<FilterStatus, QueryFilter> = {
+    all: {},
+    active: { expires_at_gt: new Date().toJSON() },
+    expired: { expires_at_lt: new Date().toJSON() },
+    never: { expires_at_null: true }
+  }
+
+  const query = useMemo<Parameters<typeof useResourceList>[0]['query']>(() => {
+    return {
       filters: {
         promotion_rule_promotion_id_eq: props.params.promotionId,
         ...(searchValue != null
           ? { code_or_coupon_recipient_email_cont: searchValue }
-          : {})
+          : {}),
+        ...(filterStatusPredicate[filterStatus] ?? {})
       },
       sort: ['-updated_at'],
       pageSize: 25
     }
+  }, [filterStatus, searchValue])
+
+  const { list, ResourceList } = useResourceList({
+    type: 'coupons',
+    query
   })
 
   return (
@@ -80,10 +108,53 @@ function Page(
       </Spacer>
 
       <Spacer top='10'>
+        <Dropdown
+          className='flex justify-end'
+          key={filterStatus}
+          dropdownItems={filterStatusList.map((status) => (
+            <DropdownItem
+              key={status}
+              label={filterStatusLabel[status]}
+              onClick={() => {
+                setFilterStatus(status)
+              }}
+              icon={filterStatus === status ? 'check' : 'keep-space'}
+            />
+          ))}
+          dropdownLabel={
+            <button className='items-center flex gap-2'>
+              <Text
+                variant='info'
+                size='small'
+                className='flex gap-1 items-center'
+              >
+                View:{' '}
+                <Text
+                  variant='primary'
+                  weight='semibold'
+                  size='small'
+                  className='flex gap-1 items-center'
+                >
+                  {filterStatusLabel[filterStatus]}
+                </Text>
+                <Icon name='caretDown' />
+              </Text>
+            </button>
+          }
+        />
+      </Spacer>
+
+      <Spacer top='4' bottom='8'>
         <div
           style={{
             borderWidth: '1px',
-            borderBottom: 0
+            borderBottom: 0,
+            borderRadius: 'var(--radius)',
+            overflow: 'hidden',
+            borderColor:
+              list?.length == null || list.length > 0
+                ? undefined
+                : 'transparent'
           }}
         >
           <ResourceList
