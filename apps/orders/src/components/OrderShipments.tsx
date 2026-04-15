97c069d93fc8a4d083bfe84b2e26205b7f339631
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2026-04-15 15:25:53 +0200

    fix: show shipment parcels tracking details in order details page

--- apps/orders/src/components/OrderShipments.tsx
+++ apps/orders/src/components/OrderShipments.tsx
@@ -1,12 +1,18 @@
 import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  Icon,
   ResourceListItem,
   Section,
   useAppLinking,
   useTokenProvider,
+  useTrackingDetails,
   useTranslation,
   withSkeletonTemplate
 } from '@commercelayer/app-elements'
-import type { Order, Shipment } from '@commercelayer/sdk'
+import type { Order, Parcel, Shipment } from '@commercelayer/sdk'
+import type { FC } from 'react'
 import type { SetNonNullable, SetRequired } from 'type-fest'
 
 interface Props {
@@ -28,12 +34,82 @@ const OrderShipment = ({
       })
     : {}
 
+  const parcelsWithTracking =
+    shipment.parcels?.filter((parcel) => parcel.tracking_number) ?? []
+
   return (
-    <ResourceListItem
-      key={shipment.id}
-      resource={shipment}
-      {...navigateToShipment}
-    />
+    <div className='flex items-center'>
+      <ResourceListItem
+        key={shipment.id}
+        resource={shipment}
+        {...navigateToShipment}
+        rightContentOverride={parcelsWithTracking.length > 0 ? null : undefined}
+      />
+      <ShipmentTrackings parcels={parcelsWithTracking} />
+    </div>
+  )
+}
+
+const ShipmentTrackings: FC<{ parcels?: Parcel[] | null }> = ({ parcels }) => {
+  if (parcels == null || parcels.length === 0) {
+    return null
+  }
+
+  return (
+    <div
+      className='flex items-center border-b border-gray-100'
+      style={{
+        alignSelf: 'stretch'
+      }}
+    >
+      {parcels.length === 1 && parcels[0] != null ? (
+        <ShipmentParcelTrackingItem as='button' parcel={parcels[0]} />
+      ) : (
+        <Dropdown
+          dropdownLabel={
+            <Button type='button' variant='circle'>
+              <Icon name='dotsThreeVertical' />
+            </Button>
+          }
+          dropdownItems={parcels.map((parcel) => (
+            <ShipmentParcelTrackingItem
+              key={parcel.id}
+              parcel={parcel}
+              as='dropdownItem'
+            />
+          ))}
+        />
+      )}
+    </div>
+  )
+}
+
+const ShipmentParcelTrackingItem: FC<{
+  parcel: Parcel
+  as: 'button' | 'dropdownItem'
+}> = ({ parcel, as }) => {
+  const { TrackingDetailsModal, openTrackingDetails } =
+    useTrackingDetails(parcel)
+
+  return (
+    <>
+      <TrackingDetailsModal />
+      {as === 'button' ? (
+        <Button
+          type='button'
+          variant='secondary'
+          size='small'
+          onClick={openTrackingDetails}
+        >
+          Tracking
+        </Button>
+      ) : (
+        <DropdownItem
+          label={parcel.tracking_number ?? parcel.id}
+          onClick={openTrackingDetails}
+        />
+      )}
+    </>
   )
 }
 
