3340009f1f4dbc09f4017996a14163e39eaf1d1d
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2026-08-05 18:13:26 +0200

    fix(promotions): fix promo check feedback

--- apps/promotions/src/pages/PromotionDetailsPage.tsx
+++ apps/promotions/src/pages/PromotionDetailsPage.tsx
@@ -346,7 +346,7 @@ function SectionCheck({
                     : "So sad, it doesn't match 😢"}
               </Spacer>
             </Text>
-            {results.data.map((rule: any, index: number) => (
+            {results.data?.map((rule: any, index: number) => (
               <CheckItem key={rule.id} index={index} rule={rule} />
             ))}
           </Spacer>
