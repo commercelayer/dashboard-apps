b7998ef2a3efa3153b674ff154de922e751745eb
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2026-08-07 18:30:11 +0200

    chore(exports): improve exports filters empty state

--- apps/exports/src/components/Details/ExportDetails.tsx
+++ apps/exports/src/components/Details/ExportDetails.tsx
@@ -17,6 +17,9 @@ export const ExportDetails = withSkeletonTemplate(({ isLoading }) => {
     return null
   }
 
+  const showIncludes = data.includes != null && data.includes.length > 0
+  const showOptions = data.dry_data === true || !isEmpty(data.fields)
+
   return (
     <ListDetails title="Info" isLoading={isLoading}>
       <Card
@@ -26,35 +29,44 @@ export const ExportDetails = withSkeletonTemplate(({ isLoading }) => {
         className="flex flex-col gap-2 mt-6 print:p-4 print:rounded-sm"
       >
         <FiltersPreview filters={data.filters} />
-        {data.includes != null && data.includes.length > 0 && (
-          <div className="flex flex-wrap items-center gap-2 px-1">
-            <Text size="small" variant="info" className="font-mono">
+        {showIncludes && (
+          <div className="flex gap-2 px-1">
+            <Text
+              size="small"
+              variant="info"
+              wrap="nowrap"
+              className="font-mono"
+            >
               Includes:
             </Text>
-            <Text size="small" className="font-mono ">
-              {data.includes.map((inc, idx) => (
-                <span key={inc} className="mr-2">
-                  {inc}
-                  {idx < (data.includes ?? []).length - 1 ? "," : ""}
-                </span>
-              ))}
+            <Text size="small" className="font-mono">
+              <div className="flex flex-wrap" style={{ columnGap: "0.5rem" }}>
+                {data.includes?.map((inc, idx) => (
+                  <span key={inc} style={{ overflowWrap: "normal" }}>
+                    {inc}
+                    {idx < (data.includes ?? []).length - 1 ? "," : ""}
+                  </span>
+                ))}
+              </div>
             </Text>
           </div>
         )}
-        {(data.dry_data !== true || !isEmpty(data.fields)) && (
-          <div className="flex flex-wrap items-center gap-2 px-1">
-            <Text size="small" variant="info" className="font-mono">
+        {showOptions && (
+          <div className="flex gap-2 px-1">
+            <Text
+              size="small"
+              variant="info"
+              wrap="nowrap"
+              className="font-mono"
+            >
               Options:
             </Text>
-            <Text size="small" className="font-mono ">
+            <Text size="small" className="font-mono">
               {data.dry_data === true && <span>importable</span>}
               {data.dry_data === true && !isEmpty(data.fields) && (
                 <span className="mr-2">,</span>
               )}
               {!isEmpty(data.fields) && <span>simple format</span>}
-              {data.dry_data !== true && isEmpty(data.fields) && (
-                <Text variant="disabled">&#8212;</Text>
-              )}
             </Text>
           </div>
         )}
