80af3bffc96a3bcd46d85120028d9613e1793ed3
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2026-08-07 14:08:29 +0200

    chore(exports): hide detail info blocks if empty

--- apps/exports/src/components/Details/ExportDetails.tsx
+++ apps/exports/src/components/Details/ExportDetails.tsx
@@ -26,40 +26,38 @@ export const ExportDetails = withSkeletonTemplate(({ isLoading }) => {
         className="flex flex-col gap-2 mt-6 print:p-4 print:rounded-sm"
       >
         <FiltersPreview filters={data.filters} />
-
-        <div className="flex flex-wrap items-center gap-2 px-1">
-          <Text size="small" variant="info" className="font-mono">
-            Includes:
-          </Text>
-          <Text size="small" className="font-mono ">
-            {data.includes != null && data.includes.length > 0 ? (
-              data.includes.map((inc, idx) => (
+        {data.includes != null && data.includes.length > 0 && (
+          <div className="flex flex-wrap items-center gap-2 px-1">
+            <Text size="small" variant="info" className="font-mono">
+              Includes:
+            </Text>
+            <Text size="small" className="font-mono ">
+              {data.includes.map((inc, idx) => (
                 <span key={inc} className="mr-2">
                   {inc}
                   {idx < (data.includes ?? []).length - 1 ? "," : ""}
                 </span>
-              ))
-            ) : (
-              <Text variant="disabled">&#8212;</Text>
-            )}
-          </Text>
-        </div>
-
-        <div className="flex flex-wrap items-center gap-2 px-1">
-          <Text size="small" variant="info" className="font-mono">
-            Options:
-          </Text>
-          <Text size="small" className="font-mono ">
-            {data.dry_data === true && <span>importable</span>}
-            {data.dry_data === true && !isEmpty(data.fields) && (
-              <span className="mr-2">,</span>
-            )}
-            {!isEmpty(data.fields) && <span>simple format</span>}
-            {data.dry_data !== true && isEmpty(data.fields) && (
-              <Text variant="disabled">&#8212;</Text>
-            )}
-          </Text>
-        </div>
+              ))}
+            </Text>
+          </div>
+        )}
+        {(data.dry_data !== true || !isEmpty(data.fields)) && (
+          <div className="flex flex-wrap items-center gap-2 px-1">
+            <Text size="small" variant="info" className="font-mono">
+              Options:
+            </Text>
+            <Text size="small" className="font-mono ">
+              {data.dry_data === true && <span>importable</span>}
+              {data.dry_data === true && !isEmpty(data.fields) && (
+                <span className="mr-2">,</span>
+              )}
+              {!isEmpty(data.fields) && <span>simple format</span>}
+              {data.dry_data !== true && isEmpty(data.fields) && (
+                <Text variant="disabled">&#8212;</Text>
+              )}
+            </Text>
+          </div>
+        )}
       </Card>
     </ListDetails>
   )
